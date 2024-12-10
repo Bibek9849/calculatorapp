9702bebc801a12fcdd5cb9fc6fd3f8238d4033ff
Bibek Pandey <pandeybibe098k@gmail.com>
2024-12-10 11:27:44 +0545

    calculator logic

---
+++ lib/view/calculator_view.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class CalculatorView extends StatefulWidget {
+  const CalculatorView({super.key});
+
+  @override
+  State<CalculatorView> createState() => _CalculatorViewState();
+}
+
+class _CalculatorViewState extends State<CalculatorView> {
+  final _textController = TextEditingController();
+  List<String> lstSymbols = [
+    "C",
+    "*",
+    "/",
+    "<-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "7",
+    "8",
+    "9",
+    "*",
+    "%",
+    "0",
+    ".",
+    "=",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Bibek Calculator App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _textController,
+              readOnly: true,
+              textAlign: TextAlign.right,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              style: const TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                ),
+                itemCount: lstSymbols.length,
+                itemBuilder: (context, index) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 166, 233, 244),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                    ),
+                    onPressed: () {
+                      String symbol = lstSymbols[index];
+                      setState(() {
+                        if (symbol == "C") {
+                          _textController.text = "";
+                        } else if (symbol == "<-") {
+                          if (_textController.text.isNotEmpty) {
+                            _textController.text = _textController.text
+                                .substring(0, _textController.text.length - 1);
+                          }
+                        } else if (symbol == "=") {
+                          try {
+                            // Replace % with /100 in the expression
+                            String input =
+                                _textController.text.replaceAll('%', '/100');
+                            Parser p = Parser();
+                            Expression exp = p.parse(input);
+                            ContextModel cm = ContextModel();
+                            double eval = exp.evaluate(EvaluationType.REAL, cm);
+                            _textController.text = eval.toString();
+                          } catch (e) {
+                            _textController.text = "Error";
+                          }
+                        } else {
+                          _textController.text += symbol;
+                        }
+                      });
+                    },
+                    child: Text(
+                      lstSymbols[index],
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
